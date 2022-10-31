b97743b1c8f5da1fb95a89c33a02d5c46e80860d
Lucas <lucasfrutoszarate@hotmail.com>
2022-10-31 07:35:42 -0400

    agg metodo para cachear

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
 //console.log('service worker......');
 
+const cacheName = "app-shell-rs";
+const assets = [
+    '/',
+    'index.html',
+    'js/app.js',
+    'js/common.js',
+    'js/materialize.min.js',
+    'css/styles.css',
+    'css/materialize.min.css',
+    'img/pkcontacts.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+caches.open(cacheName).then(cache =>{
+    cache.addAll(assets);
+})
+.catch(ev =>{
+    console.log('Error en cacheo', ev);
+});
+
+
 //install sw
 self.addEventListener('install', ev => {
     console.log('sw instalado');
@@ -9,3 +29,9 @@ self.addEventListener('install', ev => {
 self.addEventListener('activate', evt=>{
     console.log('sw activado');
 });
+
+// evento fetch 
+self.addEventListener('fetch', evt =>{
+    console.log(evt);
+});
+
