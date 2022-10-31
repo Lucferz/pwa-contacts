5381ecbab3e9342a8df09d229ae6101baa79be1f
Lucas <lucasfrutoszarate@hotmail.com>
2022-10-31 07:14:51 -0400

    Add listener install sw && IOS support tags

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-console.log('service worker......');
+//console.log('service worker......');
+
+//install sw
+self.addEventListener('install', ev => {
+    console.log('sw instalado');
+});
+
+//activar evento
+self.addEventListener('activate', evt=>{
+    console.log('sw activado');
+});
